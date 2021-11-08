c2fa76c9535d237e83ca06a75249e67446c45bdb
cto70 <yarovich2004@yandex.ru>
2021-11-08 22:52:08 +0300

    done

---
+++ GoogleTests/CorrectFraction.h
@@ -0,0 +1,41 @@
+//
+// Created by yarov on 02.11.2021.
+//
+#ifndef LAB6_CORRECTFRACTION_H
+#define LAB6_CORRECTFRACTION_H
+#include <iostream>
+#include <exception>
+#include <cmath>
+#include <string>
+#include <vector>
+using std :: string;
+using std :: vector;
+class CorrectFraction
+{
+private:
+    long long numerator;
+    long long denominator;
+private:
+    CorrectFraction();
+public:
+    long long GCD( const long long num, const long long den );
+    string IntToString( long long a );
+    void StandartView();
+    void assign( long long  numerator_, long long denominator_ );
+public:
+    CorrectFraction( const long long numerator_, const long long denominator_ );
+    CorrectFraction( const CorrectFraction& CorrectFraction );
+public:
+    static CorrectFraction SUM( CorrectFraction a, CorrectFraction b );
+    static CorrectFraction MULTIPLY( CorrectFraction a, CorrectFraction b );
+    static CorrectFraction DIVIDE( CorrectFraction a, CorrectFraction b );
+public:
+    void SUM( CorrectFraction a );
+    void MULTIPLY( CorrectFraction a );
+    void DIVIDE( CorrectFraction a );
+public:
+    long long getNumerator();
+    long long getDenominator();
+    string to_string();
+};
+#endif //LAB6_CORRECTFRACTION_H
