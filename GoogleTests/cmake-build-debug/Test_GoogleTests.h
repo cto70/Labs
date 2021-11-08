e5d79f0e9fa8823581769516938f0aab7a9b96d4
cto70 <37798870+cto70@users.noreply.github.com>
2021-11-08 23:07:14 +0300

    Delete GoogleTests directory

--- GoogleTests/cmake-build-debug/Test_GoogleTests.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by yarov on 02.11.2021.
-//
-#include "gtest/gtest.h"
-#ifndef GOOGLETEST_TEST_GOOGLETESTS_H
-#define GOOGLETEST_TEST_GOOGLETESTS_H
-
-
-#endif //GOOGLETEST_TEST_GOOGLETESTS_H
